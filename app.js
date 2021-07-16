1d5a3d6f039d97d9f70e2295d9e5bb2bcbf4d398
vlauncher <68847935+vlauncher@users.noreply.github.com>
2021-07-16 12:34:34 +0100

    Made deployments updates

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Use Routes
 app.use('/',IndexRouter)
 //app port
-app.listen(3000,(err)=>{
+app.listen(process.env.PORT || 3000,(err)=>{
   if(err){
     console.log(err);
   }else{
